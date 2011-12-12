9770ba986031a00fb0da7e0e72471e7af7a0218e
geniuslinda <geniuslinda@gmail.com>
2011-12-12 13:57:22 -0800

    Linda - add missing methods for IFeatureToggleFactory, also upgrade the version

--- nToggle/IFeatureToggleFactory.cs
+++ nToggle/IFeatureToggleFactory.cs
@@ -4,5 +4,7 @@
     {
         IFeatureToggle GetFeatureToggle(string featureName, bool reversed);
         IFeatureToggle GetFeatureToggle(string featureName);
+        IConditionFeatureToggle GetConditionFeatureToggle(string featureName, bool reversed);
+        IConditionFeatureToggle GetConditionFeatureToggle(string featureName);
     }
 }
